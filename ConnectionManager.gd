0e1fbc006439a9c2969bea542b6446cefcea9be4
Devlin McClure <mccluredt@gmail.com>
2018-10-03 18:31:18 -0400

    Add parts of connection code to ConnectionManager script

--- ConnectionManager.gd
+++ ConnectionManager.gd
@@ -1,9 +1,22 @@
 extends Node
 
 const PORT = 1234
+var ship = null
+var players = []
+
+func _ready():
+	ship = preload("res://Ship.tscn")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
 
 func on_host_game():
-	pass
+	var host = NetworkedMultiplayerENet.new()
+	# 4 is the number of maximum clients
+	host.create_server(PORT, 4)
+	get_tree().set_network_peer(host)
+	
+	_connected_ok()
 
 func on_join_game(ip):
-	pass
+	var host = NetworkedMultiplayerENet.new()
+	host.create_server(ip, PORT)
+	get_tree().set_network_peer(host)

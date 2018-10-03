816f3157a579f8feae68c2bc7b7634d2220a7964
Devlin McClure <mccluredt@gmail.com>
2018-10-03 18:19:06 -0400

    Add comments to Ship script

--- Ship.gd
+++ Ship.gd
@@ -17,13 +17,17 @@ func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
 	
+	#If user presses up key, then move forward
 	if Input.is_action_pressed("ui_up"):
 		add_force(Vector2(0,0), Vector2(cos(rotation), sin(rotation)) * delta * speed)
 	
+	#limit the velocity to max speed
 	if get_linear_velocity().length() > max_speed:
 		set_linear_velocity(get_linear_velocity().normalized()*max_speed)
 	
+	#If the user presses left, then rotate left
 	if Input.is_action_pressed("ui_left"):
 		set_angular_velocity(-rotation_speed * delta)
+	#If the user presses right, then rotate right
 	if Input.is_action_pressed("ui_right"):
 		set_angular_velocity(rotation_speed * delta)

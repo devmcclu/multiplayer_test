40cbbc5ab5188773a9a0b97b5232f5372c1ae08b
Devlin McClure <mccluredt@gmail.com>
2018-10-04 13:50:26 -0400

    Finish ConnectionManager script code

--- ConnectionManager.gd
+++ ConnectionManager.gd
@@ -20,3 +20,21 @@ func on_join_game(ip):
 	var host = NetworkedMultiplayerENet.new()
 	host.create_server(ip, PORT)
 	get_tree().set_network_peer(host)
+	
+func _connected_ok():
+	rpc("register_player", get_tree().get_network_unique_id())
+	register_player(get_tree().get_network_unique_id())
+	get_tree().get_root().get_node("Lobby").queue_free()
+	# this function is called when a new player is connected
+	# note the use of the keyword remote which mean that the code will only be called on the others
+remote func register_player(player_id):
+	var p = ship.instance()
+	p.set_network_master(player_id)
+	p.name = str(player_id)
+	get_tree().get_root().add_child(p)
+	# if I'm the server I inform the new connected player about the others
+	if get_tree().get_network_unique_id() == 1:
+		if player_id != 1:
+			for i in players:
+				rpc_id(player_id, "register_player", i)
+			players.append(player_id)
